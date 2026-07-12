1048fc3b5d5f5e7747c95bdb76ebc88662532b5c
askrawe2010-max <askrawe_2010@hotmail.com>
2026-07-12 14:31:51 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Chem Lab Calc</h1>
+      <p>التطبيق شغال! 🎉</p>
+    </div>
+  )
+}
+
+export default App
